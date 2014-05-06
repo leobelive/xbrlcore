6bb0958f6a5f7d6c9ce7eda555a026e7acad8ab8
joe.phoenix <joe.phoenix@gmail.com>
2014-05-06 16:18:04 +0800

    初始化项目

---
+++ xbrlcore/src/gbicc/xbrl/validate/controller/ResultController.java
@@ -0,0 +1,37 @@
+package gbicc.xbrl.validate.controller;
+
+import java.util.List;
+
+import gbicc.xbrl.validate.service.ResultService;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 处理的控制器
+ * 
+ * @author joephoenix
+ */
+@Controller
+@Component
+public class ResultController {
+	@Resource(name = "resultService")
+	private ResultService resultService;
+
+	protected static Logger logger = Logger.getLogger("controller");
+
+	@RequestMapping(value = "/list")
+	public String IndexPage(@PathVariable Integer p, Model model) {
+		// Pageable pp = new PageRequest(p, 10);
+		List<String> result = resultService.returnErrorList();
+
+		model.addAttribute("validate_results", result);
+		return "/validateresults";
+	}
+}
